aa4ea8231869ef06a75cb3eb1186be9fd537748c
Arun Bojja <contact@arunbojja.com>
2023-02-10 15:07:21 -0600

    Updates

---
+++ precache-manifest.bf9891eb19b291795ffd370f710aafad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4edae626a121819a46e1",
+    "url": "/nlp-notes/static/css/main.a8854719.chunk.css"
+  },
+  {
+    "revision": "4edae626a121819a46e1",
+    "url": "/nlp-notes/static/js/main.4edae626.chunk.js"
+  },
+  {
+    "revision": "9f214d70663891210c4f",
+    "url": "/nlp-notes/static/js/1.9f214d70.chunk.js"
+  },
+  {
+    "revision": "f8c0ccc7c9b33170bb29",
+    "url": "/nlp-notes/static/js/2.f8c0ccc7.chunk.js"
+  },
+  {
+    "revision": "b908f015858c8ab5aef9",
+    "url": "/nlp-notes/static/js/runtime~main.b908f015.js"
+  },
+  {
+    "revision": "8a093acb63b1f9d0da5c149a94c29b1b",
+    "url": "/nlp-notes/index.html"
+  }
+];
